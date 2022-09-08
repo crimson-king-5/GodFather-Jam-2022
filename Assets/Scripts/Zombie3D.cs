6b1a23f8069a40696f22e66b0f45a394611b2b99
pgw110 <pgw110@PORT6LHQPH2.edu.atlas.local>
2022-09-08 17:34:50 +0200

    script zombie 3d

--- Assets/Scripts/Zombie3D.cs
+++ Assets/Scripts/Zombie3D.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Zombie3D : MonoBehaviour
 {
+    private Animator Anime;
+
+    private void Start()
+    {
+        Anime = GetComponent<Animator>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Human"))
         {
             //attack
+            Anime.SetTrigger("touche");
+            collision.gameObject.GetComponent<EnemyAi>().enabled = false;
+            collision.gameObject.GetComponent<Animator>().SetTrigger("Death");
+
+
         }
         else if (collision.gameObject.CompareTag("Floor"))
         {
             //lose pv
+            Anime.SetTrigger("fail");
         }
     }
 }
